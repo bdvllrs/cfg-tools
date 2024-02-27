6afb3d2c461da4995011ce25b7b4245bbd0e022c
bdvllrs <bdvllrs@gmail.com>
2024-02-27 15:15:00 +0000

    Update version

--- cfg_tools/__init__.py
+++ cfg_tools/__init__.py
@@ -1,7 +1,11 @@
+import importlib.metadata
+
 from cfg_tools import plugins
 
 from .data_parser import ParsedModel, parse_dict, parse_list, parse_str, register_plugin
 from .utils import load_config_files, merge_dicts, parse_args
+
+__version__ = importlib.metadata.version("cfg-tools")
 
 __all__ = [
     "parse_str",
